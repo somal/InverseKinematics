33ef9631b32113a5e5403f3d457ca27f6b9b0bbe
somal <somal1996>
2019-01-08 22:02:10 +0300

    Init manipulator

---
+++ Manipulator.cpp
@@ -0,0 +1,10 @@
+//
+// Created by somal on 08.01.19.
+//
+
+#include "Manipulator.h"
+
+Manipulator::Manipulator(vector<Link> *links, Connection *connection) {
+    this->links = links;
+    this->connection = connection;
+}
---
+++ Manipulator.h
@@ -0,0 +1,24 @@
+//
+// Created by somal on 08.01.19.
+//
+
+#ifndef INC_2NDTEST_MASTERSSCHOLARSHIPINROBOTICS_SOKOLOVMAXIM_MANIPULATOR_H
+#define INC_2NDTEST_MASTERSSCHOLARSHIPINROBOTICS_SOKOLOVMAXIM_MANIPULATOR_H
+
+#include <vector>
+#include "Link.h"
+#include "Connection.h"
+
+class Manipulator {
+private:
+    vector<Link> *links;
+    Connection *connection;
+
+public:
+    Manipulator(vector<Link> *links, Connection *connection);
+
+    ~Manipulator() = default;
+};
+
+
+#endif //INC_2NDTEST_MASTERSSCHOLARSHIPINROBOTICS_SOKOLOVMAXIM_MANIPULATOR_H
