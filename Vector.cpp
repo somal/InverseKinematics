11d42b97c047e1128b2e6f3dc7816eecdbc2d8bd
somal <somal1996>
2019-01-10 13:30:08 +0300

    Add initialization by points

--- Vector.cpp
+++ Vector.cpp
@@ -3,6 +3,8 @@
 //
 
 #include <cmath>
+#include <vector>
+#include <cassert>
 
 #include "Vector.h"
 
@@ -28,8 +30,19 @@ float Vector::get_length() {
     return sqrt(this->x * this->x + this->y * this->y + this->z * this->z);
 }
 
-Vector *Vector::vector_product(Vector *vector1, Vector *vector2) {
+Vector *Vector::cross_product(Vector *vector1, Vector *vector2) {
     return new Vector((vector1->y * vector2->z - vector1->z * vector2->y),
                       (vector1->x * vector2->z - vector1->z * vector2->x),
                       (vector1->x * vector2->y - vector1->x * vector2->y));
 }
+
+Vector::Vector(std::vector<float> pt_start, std::vector<float> pt_end) {
+    assert(pt_start.size() == 3);
+    assert(pt_end.size() == 3);
+
+    this->x = pt_end[0] - pt_start[0];
+    this->y = pt_end[1] - pt_start[1];
+    this->z = pt_end[2] - pt_start[1];
+}
+
+
--- Vector.h
+++ Vector.h
@@ -1,6 +1,9 @@
 #ifndef INC_2NDTEST_MASTERSSCHOLARSHIPINROBOTICS_SOKOLOVMAXIM_VECTOR_H
 #define INC_2NDTEST_MASTERSSCHOLARSHIPINROBOTICS_SOKOLOVMAXIM_VECTOR_H
 
+#include <vector>
+
+using namespace std;
 
 class Vector {
 private:
@@ -10,13 +13,15 @@ private:
 public:
     Vector(float x, float y, float z);
 
+    Vector(std::vector<float> pt_start, std::vector<float> pt_end);
+
     ~Vector() = default;
 
     float get_length();
 
     static float scalar_product(Vector *vector1, Vector *vector2);
 
-    static Vector *vector_product(Vector *vector1, Vector *vector2);
+    static Vector *cross_product(Vector *vector1, Vector *vector2);
 
     static Vector *float_multiplication(Vector *vector, float f);
 };
